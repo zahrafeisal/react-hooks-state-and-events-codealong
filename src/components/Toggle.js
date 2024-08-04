896e390e0bd2b0303468672fc22491c7402b33a3
zahrafeisal <zahrafeisal3@gmail.com>
2024-08-04 19:07:52 +0300

    added onClick, isOn state

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
-}
+  const [ isOn, setIsOn] = useState(false)
+  console.log(isOn)
+
+  function handleClick() {
+    setIsOn(isOn => !isOn)
+  }
+
+  const color = isOn ? "red" : "white"
+
+  return (
+    <button
+      style={ {background : color} }
+      onClick={handleClick}
+      >
+        {isOn ? 'ON' : 'OFF' }
+    </button>
+  );
+};
 
 export default Toggle;
